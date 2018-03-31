fd8bb81d88f29d75916c3d5f085d6e7fba48b00b
RU0DYVN5U9EVO4O\Administrator <670827368@qq.com>
2018-03-31 19:23:46 +0800

    no message

---
+++ 项目创建/GuessNumber(Third rewrite).java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scann=new Scanner(System.in);
+		
+		Random radom=new Random();
+		int AnserNumber=radom.nextInt(1000);
+		int i=1;
+		
+		
+			
+	       
+	         System.out.println("请输入你要猜的数字");	
+	         for(;;){
+        	int GuessNumber=scann.nextInt();
+			
+				
+			if(GuessNumber>AnserNumber){
+				System.out.println("大了");
+          i++;
+			}else if(GuessNumber<AnserNumber){
+			System.out.println("小了");
+			i++;
+			
+		}else{if(GuessNumber==AnserNumber){
+			System.out.println("猜对了"+"，你一个猜了"+i+"次"); 
+			
+		}
+			
+			
+		      
+		}
+			
+		}
+		
+		
+		
+	}
+}
