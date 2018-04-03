ccfcb746ac660bad9466ba0cafcd42220b2d5408
Jeavir <37451581+Jeavir@users.noreply.github.com>
2018-04-03 22:19:06 +0800

    Update GuessNumber(Third rewrite).java

--- 项目创建/GuessNumber(Third rewrite).java
+++ 项目创建/GuessNumber(Third rewrite).java
@@ -1,38 +1,51 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class GuessNB {
+public class CAIshuzhi {
+	
+public static void main(String []args){
+	   
 
-public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+      
+    	System.out.println("请输入你的昵称");
+		String Playername;
+            Scanner Gameplayer=new Scanner(System.in);
+            String  PlayerName = Gameplayer.next();
 		Scanner scann=new Scanner(System.in);
 		
 		Random radom=new Random();
-		int AnserNumber=radom.nextInt(1000);
+		int AnserNumber=radom.nextInt(10);
 		int i=1;
-		
-             System.out.println("请输入你要猜的数字");	
+		   
+	         System.out.println("请输入你要猜的数字");	
+	         long startMili=System.currentTimeMillis();
 	         for(;;){
-        int GuessNumber=scann.nextInt();
-	if(GuessNumber>AnserNumber){
- 		System.out.println("大了");
-		  i++;
-	}else if(GuessNumber<AnserNumber){
-		System.out.println("小了");
-		  i++;
+	        	 
+        	int GuessNumber=scann.nextInt();
 			
-		}else{
-	         System.out.println("猜对了"+"，你一个猜了"+i+"次"); 
+				
+			if(GuessNumber>AnserNumber){
+				System.out.println("大了");
+          i++;
+			}else if(GuessNumber<AnserNumber){
+			System.out.println("小了");
+			i++;
 			
-		}
+		}else{	System.out.println("恭喜你猜对了，"+"亲爱的"+PlayerName+"同学，你一共猜了"+i+"次"); 
+		        long endMili=System.currentTimeMillis();
+		       
+				System.out.println("总耗时为；"+(endMili-startMili)/1000+"秒");
+		continue;
 			
+		}  
 			
-		      
-		}
-			
-		}
-		
-		
-		
 	}
+				
+
+       }
+	
+	         
+	
+	}
+     
+		
